d02699b7b2c4e83bddb5e295ced1970ca4f6a76f
zecht12 <gilangprimae13@gmail.com>
2024-04-28 16:19:06 +0700

    styling

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                 </div>
                 <div>
                     <ul className='flex xl:hidden md:hidden sm:hidden xs:hidden'>
-                        <li className={cn('md:ml-10 rounded-full bg-slate-100 hover:bg-blue-400')}>
+                        <li className={cn('ml-10 rounded-full bg-slate-100 hover:bg-blue-400')}>
                             <Link href='#membership'>
                                 <p className={cn('text-sm text-black uppercase md:px-8 px-4 py-2', button.className)}>Daftar</p>
                             </Link>
