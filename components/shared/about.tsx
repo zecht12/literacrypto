67fac864a83b73107c855a4e475be94158ea50f4
zecht12 <gilangprimae13@gmail.com>
2024-04-28 10:30:13 +0700

    push about changes

--- components/shared/about.tsx
+++ components/shared/about.tsx
@@ -4,41 +4,51 @@ import { cn } from "../../lib/utils";
 import { Poppins, Montserrat, League_Spartan, Open_Sans } from "next/font/google";
 import Image from "next/image";
 
-const head = Poppins({
+const head = League_Spartan({
     subsets: ["latin"],
-    weight: ["700"]
+    weight: ["800"]
 });
 
 const subHead = Poppins({
     subsets: ["latin"],
-    weight: ["500"]
-});
+    weight: ["700"]
+})
 
-const body = Poppins({
+const body = Montserrat({
     subsets: ["latin"],
     weight: ["400"]
 });
+
+const bodyBold = Montserrat({
+    subsets: ["latin"],
+    weight: ["500"]
+});
+
+const button = Open_Sans({
+    subsets: ["latin"],
+    weight: ["500"]
+})
 
 const About = () => {
     return (
         <div className="w-full md:h-screen h-auto xl:flex md:flex justify-between items-center mx-auto xl:px-16 md:px-12 sm:px-8 px-6 bg-black">
             <div className="max-w-lg">
-                <p className={cn("text-[#b98614] text-3xl xl:pt-8 md:pt-6 sm:pt-5 pt-12 md:text-start text-center ", head.className)}>
-                    Let's talk with us
+                <p className={cn("text-[#b98614] text-5xl xl:pt-8 md:pt-6 sm:pt-5 pt-12 md:text-start text-center ", head.className)}>
+                    Let's Talk With Us
                 </p>
-                <p className={cn("text-slate-100 text-base text-justify xl:pt-8 md:pt-6 sm:pt-5 pt-4", body.className)}>
-                    Kami literacrypto tidak memperjual belikan website kami dimanapun Dan dengan siapapun. Tidak diperkenankan untuk menggandakan, mendistribusikan, atau menggunakan konten kami tanpa izin tertulis dari LiteraCrypto.
+                <p className={cn("text-slate-100 text-base text-justify xl:pt-4 md:pt-3 sm:pt-2 pt-2", body.className)}>
+                    Kami literacrypto tidak memperjual belikan website kami dimanapun dan dengan siapapun. Tidak diperkenankan untuk menggandakan, mendistribusikan, atau menggunakan konten kami tanpa izin tertulis dari LiteraCrypto.
                 </p>
-                <p className={cn("text-[#b98614] text-3xl xl:py-8 md:py-6 sm:py-5 py-4 md:text-start text-center", subHead.className)}>
+                <p className={cn("text-[#b98614] text-2xl xl:pt-4 md:pt-3 sm:pt-2 pt-2 md:text-start text-center", subHead.className)}>
                     Instagram
                 </p>
-                <p className={cn("text-slate-100 text-base md:text-start text-center", body.className)}>
+                <p className={cn("text-slate-100 text-base md:text-start text-center xl:pt-4 md:pt-3 sm:pt-2 pt-2", bodyBold.className)}>
                     @literacrypto
                 </p>
-                <p className={cn("text-[#b98614] text-3xl xl:py-8 md:py-6 sm:py-5 py-4 md:text-start text-center", subHead.className)}>
+                <p className={cn("text-[#b98614] text-2xl xl:pt-4 md:pt-3 sm:pt-2 pt-2 md:text-start text-center", subHead.className)}>
                     X
                 </p>
-                <p className={cn("text-slate-100 text-base md:text-start text-center", body.className)}>
+                <p className={cn("text-slate-100 text-base xl:pt-4 md:pt-3 sm:pt-2 pt-2 md:text-start text-center", bodyBold.className)}>
                     @literacrypto
                 </p>
             </div>
--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,10 +1,10 @@
-import { Poppins } from 'next/font/google';
+import { Montserrat, Poppins } from 'next/font/google';
 import React from 'react'
 import { cn } from '../../lib/utils';
 
-const subHead = Poppins({
+const body = Montserrat({
     subsets: ["latin"],
-    weight: ["500"]
+    weight: ["700"]
 });
 
 const Footer = () => {
@@ -12,7 +12,7 @@ const Footer = () => {
         <div>
             <div className='w-auto h-auto bg-black'>
                 <div className='flex w-full h-full p-2 justify-center'>
-                    <p className={cn('font-bold text-slate-100', subHead.className)}>Copyright @ 2024 LiteraCrypto</p>
+                    <p className={cn('font-bold text-slate-100', body.className)}>Copyright @ 2024 LiteraCrypto</p>
                 </div>
             </div>
         </div>
