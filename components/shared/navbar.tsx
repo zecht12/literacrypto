ad16f862f8903329d490fdcaadce3d2d6baade27
zecht12 <gilangprimae13@gmail.com>
2024-04-28 16:17:24 +0700

    push bug

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { cn } from '../../lib/utils'
 import { Open_Sans } from 'next/font/google'
+import { Router } from 'next/router'
 
 const button = Open_Sans({
     subsets: ["latin"],
@@ -13,15 +16,29 @@ const button = Open_Sans({
 
 
 export default function Navbar() {
+    useEffect(() => {
+
+        const routerEvent = 'beforeHistoryChange';
+
+        const evntHandler = () => {
+            window.scroll(0, 0);
+        };
+
+        Router.events.on(routerEvent, evntHandler);
+
+        return () => {
+            Router.events.off(routerEvent, evntHandler);
+        };
+    }, []);
     return (
-        <div className=" fixed w-full shadow-xl h-20 z-[100]">
+        <div className=" fixed xl:w-full md:w-full sm:w-full flex justify-center items-center w-auto h-20 z-[100]">
             <div className='flex justify-between items-center w-full h-full px-6 2xl:px-16 '>
                 <div className='flex items-center justify-start'>
                     <Image src="/images/logo.png" alt="image" width={40} height={40} className='rounded-full' />
                     <h1 className='font-CodecProExtraBold md:text-2xl text-slate-100'>LiteraCrypto</h1>
                 </div>
                 <div>
-                    <ul className='flex md:hidden'>
+                    <ul className='flex xl:hidden md:hidden sm:hidden'>
                         <li className={cn('md:ml-10 rounded-full bg-slate-100 hover:bg-blue-400')}>
                             <Link href='#membership'>
                                 <p className={cn('text-sm text-black uppercase md:px-8 px-4 py-2', button.className)}>Daftar</p>
@@ -30,7 +47,7 @@ export default function Navbar() {
                     </ul>
                 </div>
                 <div>
-                    <ul className='md:flex hidden justify-between items-center gap-8'>
+                    <ul className='xl:flex md:flex sm:flex hidden justify-between items-center gap-8'>
                         <li className='md:ml-10 text-sm font-CodecProBold text-slate-100 uppercase hover:text-blue-400 '>
                             <Link href='/'>
                                 <p>HOME</p>
